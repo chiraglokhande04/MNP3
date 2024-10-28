860d2cd7841f00a8ee01dcd9a4abbb9119978476
chiraglokhande04 <chiraglokhande14@gmail.com>
2024-10-28 23:05:58 +0530

    fifth commit

--- Backend/app.py
+++ Backend/app.py
@@ -8,7 +8,7 @@ import PyPDF2
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://localhost:5173/", "methods": ["GET", "POST"]}})
+CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST"]}})
 
 # ---- Job Recommendations ---- #
 
